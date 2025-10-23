62ea5f19f4145f9d777a804a28762b3b6b3c69fa
koghalai123 <koghalai@gmail.com>
2025-10-23 16:04:08 -0700

    getting stuff working on my new computer

--- launchCalibrationPhysical.sh
+++ launchCalibrationPhysical.sh
@@ -1,5 +1,5 @@
 ros2 run realsense2_camera realsense2_camera_node &
-/home/koghalai/ar4_ws/venv/bin/python /home/koghalai/ar4_ws/src/ar4_practice/scripts/arucoTesting.py &
+python /home/koghalai/ar4_ws/src/ar4_practice/scripts/arucoTesting.py &
 ros2 launch annin_ar4_driver driver.launch.py calibrate:=True include_gripper:=False &
 sleep 2
 #ros2 launch annin_ar4_moveit_config moveit.launch.py use_sim_time:=true include_gripper:=False
